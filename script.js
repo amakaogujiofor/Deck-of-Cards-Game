8e2d7175c50f100f08a195cc81184cd4ca38d020
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-01 12:57:43 +0100

    Updated script file
    Added more code blocks to fetch API data

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 // Declarations
-cardsDeck = document.getElementById("new-cards");
+let deckId;
+
+// Function Declaration and API Details
+function getCards() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
+}
+
+document.getElementById("new-cards").addEventListener("click", getCards);
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      document.getElementById("cards").innerHTML = `
+                    <img src=${data.cards[0].image} />
+                    <img src=${data.cards[1].image} />
+                `;
+    });
+});
